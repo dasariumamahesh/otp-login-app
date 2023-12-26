167f24d2d9ea9253a90a3b2c09e04460a9bebdbe
dasariumamahesh <um.dasari24@gmail.com>
2023-12-26 16:12:49 +0530

    add cord

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const routes = require('./routes/routes');
 require('dotenv').config();
 const port = process.env.PORT || 3000;
-
+app.use(cors());
 app.use(express.json());
 
 app.use('/', routes);
 
-app.listen(port, async ()=> {
+app.listen(port, async () => {
     console.log(`Listening on port: ${port}`);
-})
+});
